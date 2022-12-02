b099645464b8bf11a46651d56abe501c7e417b8f
mohamadfakir <113498626+mohamadfakir@users.noreply.github.com>
2022-12-02 15:18:15 +0530

    Add  Second largest

--- src/arrayproblems/Secondlargest.java
+++ src/arrayproblems/Secondlargest.java
@@ -3,6 +3,7 @@ package arrayproblems;
 import java.util.Scanner;
 
 public class Secondlargest {
+    
     public static void main(String[] args) {
         System.out.println("enter the number of elements you want to enter\n");
         Scanner n = new Scanner(System.in);
