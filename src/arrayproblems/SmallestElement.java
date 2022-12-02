c25a4ea3c600eef37463032d7a9aa32b2b93a83d
mohamadfakir <113498626+mohamadfakir@users.noreply.github.com>
2022-12-02 15:19:11 +0530

    Add  Smallest Element

--- src/arrayproblems/SmallestElement.java
+++ src/arrayproblems/SmallestElement.java
@@ -3,6 +3,7 @@ package arrayproblems;
 import java.util.Scanner;
 
 public class SmallestElement {
+    
     public static void main(String[] args) {
 
         System.out.println("enter the number of elements you want to enter\n");
